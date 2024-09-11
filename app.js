e1f435b2b312061b59cc269003de50b54abfd5e0
Ziqing19 <mzq5176@gmail.com>
2024-09-11 14:40:03 +0800

    add cors

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const fetch = require("node-fetch");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: false, limit: "20mb" }));
+app.use(cors({
+  origin: /[^.]+\.arkrec\.com/
+}));
 
 app.post("/proxy", async (req, res) => {
   const data = req.body;
